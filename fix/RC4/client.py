1a7b9bfdc758d23ecd02735a4957b5e01821b202
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-10-09 23:56:13 +0700

    update files

--- fix/RC4/client.py
+++ fix/RC4/client.py
@@ -1,8 +1,5 @@
 import socket
 from Crypto.Cipher import ARC4
-from Crypto.Util.Padding import pad
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Random import get_random_bytes
 
 sock = socket.socket()
 sock.connect(('127.0.0.1', 8080))
