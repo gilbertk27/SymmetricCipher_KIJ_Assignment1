74df60940f033c8d92a1e8583e5c56868ff78bb5
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-10-09 22:13:25 +0700

    update files

---
+++ AES/3_decrypt
@@ -0,0 +1,25 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+#CBC
+input_file = 'encryptedCBC.bin' # Input file
+
+# Read the data from the file
+file_in = open(input_file, 'rb') 
+iv = file_in.read(16) 
+ciphered_data = file_in.read()
+file_in.close()
+
+cipher = AES.new(key, AES.MODE_CBC, iv=iv) 
+st_dec = time.time()
+original_data = unpad(cipher.decrypt(ciphered_data), AES.block_size) 
+et_dec = time.time()
+elapsed_time_dec = et_dec - st_dec
+final_res_dec = elapsed_time_dec * 1000 # milisecond
+
+file_out = open("decryptedCBC.txt", "wb") 
+file_out.write(original_data)
+file_out.close()
+
+print ("Decrypted: %r" % original_data)
+print ("Elapsed time (Decrypted): %r ms"  % final_res_dec)
--- RC4/2_rc4.py
+++ RC4/2_rc4.py
@@ -1,4 +1,3 @@
-# FIXED POL RC4
 from Crypto.Cipher import ARC4
 from Crypto.Hash import SHA256
 from Crypto import Random
@@ -9,22 +8,29 @@ def enc(key,data):
 def dec(key,msg):
 		return ARC4.new(key).decrypt(msg)
 
-def main():
-  key = ''
-  data = ''
-  with open('keygen.key', 'rb') as file:
-    key = file.read()    
-    with open('input2.txt', 'rb') as file: 
-      data = file.read()
+key = ''
+data = b'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. N' # Must be a bytes object
+with open('keygen.key', 'rb') as file:
+  key = file.read()
+# with open('input2.txt', 'rb') as file: 
+  # data = file.read()
 
-      encrypted = enc(key,data)
-      enc_file = open("encrypted.enc", "wb")
-      enc_file.write(encrypted)
-      # print(encrypted)
+st_enc = time.time()
+encrypted = enc(key,data)
+et_enc = time.time()
+elapsed_time_enc = et_enc - st_enc
+final_res_enc = elapsed_time_enc * 1000 # milisecond
+enc_file = open("encrypted.enc", "wb")
+enc_file.write(encrypted)
+print ("Encrypted: %r" % encrypted)
+print ("Elapsed time (Encrypted): %r ms \n"  % final_res_enc)
 
-      decrypted = dec(key,encrypted)
-      enc_file = open("decrypted.txt", "wb")
-      enc_file.write(decrypted)
-
-if __name__=='__main__':
-  main()
+st_dec = time.time()
+decrypted = dec(key,encrypted)
+et_dec = time.time()
+elapsed_time_dec = et_dec - st_dec
+final_res_dec = elapsed_time_dec * 1000 # milisecond
+dec_file = open("decrypted.txt", "wb")
+dec_file.write(decrypted)
+print ("Decrypted: %r" % decrypted)
+print ("Elapsed time (Decrypted): %r ms"  % final_res_dec)
